38d0330f8e93e8f0f7135cdb0c7550a02fe55848
Hoang Le <43483151+Soos99@users.noreply.github.com>
2018-12-03 02:26:12 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+            Game game = new Game();
+
+            JFrame window = new JFrame("2048");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setResizable(false);
+            window.add(game);
+            window.pack();
+            window.setLocationRelativeTo(null);
+            window.setVisible(true);
+            game.start();
+        }
+}
